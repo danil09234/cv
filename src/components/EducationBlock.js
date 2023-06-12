3df82447cc8a7693620a976f817429cc45a1c6c7
danil09234 <dailzag05@gmail.com>
2023-06-12 18:53:44 +0200

    Refactoring

--- src/components/EducationBlock.js
+++ src/components/EducationBlock.js
@@ -28,18 +28,39 @@ function DataLine({title, specialization, place}) {
 
 
 function EducationBlock() {
+    let education_list = [
+        {
+            "title": "Technical university of košice",
+            "specialization": "Faculty of electrical engineering and informatics, specialization “Informatics”",
+            "place": "Košice, Slovensko"
+        },
+
+        {
+            "title": "Technical college “ZETK ZNTU”",
+            "specialization": "Specialization software development",
+            "place": "Zaporizhzhia, Ukraine"
+        },
+
+        {
+            "title": "IT STEP Junior academy",
+            "specialization": null,
+            "place": "Zaporizhzhia, Ukraine"
+        },
+
+        {
+            "title": "Middle & high school №108",
+            "specialization": null,
+            "place": "Zaporizhzhia, Ukraine"
+        }
+    ]
+
+    education_list = education_list.map(({title, specialization, place}, index) => {
+        return <DataLine key={index} title={title} specialization={specialization} place={place} />;
+    })
+
     return (
         <div className={styles.content}>
-            <DataLine title="Technical university of košice"
-                      specialization="Faculty of electrical engineering and informatics, specialization “Informatics”"
-                      place="Košice, Slovensko" />
-            <DataLine title="technical collage “ZETK ZNTU”"
-                      specialization="specialization software development"
-                      place="Zaporizhzhia, Ukraine" />
-            <DataLine title="IT STEP Junior academy"
-                      place="Zaporizhzhia, Ukraine" />
-            <DataLine title="Middle & high school №108"
-                      place="Zaporizhzhia, Ukraine" />
+            {education_list}
         </div>
     );
 }
